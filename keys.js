b4f76fddf4d0efb085e67813d555f440fe610547
Tyler Hunter <tyler.a.hunter@gmail.com>
2018-03-15 01:53:11 -0400

    liri bot

--- keys.js
+++ keys.js
@@ -1,15 +1,24 @@
 //sharing this value with any object
-
+//
 exports.spotifyKeys = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: 'f5ee1b1ce6d5439db48524361285519e',
+    secret: 'd233e8fe5bd44f698d1efd8c260ea7e0',
 };
+//
+// var twitterKeys = {
+//
+// }
+//
+// module.exports = {
+//     spotify: spotifyKeys,
+//     twitter: twitterKeys
+// }
 
-var twitterKeys = {
+console.log("this is loaded");
 
-}
-
-module.exports = {
-    spotify: spotifyKeys,
-    twitter: twitterKeys
+exports.twitterKeys= {
+    consumer_key: 'FZc34EY8UNXbIuae1sWtf3X46',
+    consumer_secret: 'ZH9sUEIqMrzgmW2SEsqUtO5UBij5TcUJvZOaFqL9tZxFh8RwTu',
+    access_token_key: '874128552597258240-SzkoGKnktb31Ca8yZVk5ikYqzhTi1V7',
+    access_token_secret: 'v8clFFIhkfsQ8vKfZ3zsGD0s49qIrr2vsuAR7GibDNCt4',
 }
--- liri.js
+++ liri.js
@@ -1,25 +1,124 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-//requiring what 'key.js' is sharing
-//the variable is equal to the object created in keys.js
-var keys = require('./keys');
+var keys = require('./keys.js');
+
+var Twitter = require('twitter');
 
 var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify({
-    id: keys.id,
-    secret: keys.secret
-});
+var request = require('request');
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
+var fs = require("fs");
+
+//requiring what 'key.js' is sharing
+//the variable is equal to the object created in keys.js
+// var keys = require('./keys');
+
+var getArtistNames = function(artist) {
+    return artist.name;
+}
+var getMeSpotify = function (songName) {
+
+    var spotify = new Spotify(keys.spotifyKeys);
+
+    spotify.search({type: 'track', query: songName}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        var songs = data.tracks.items;
+        for(var i=0; i<songs.length; i++) {
+            console.log(i);
+            console.log('artist(s): ' + songs[i].artists.map(
+                getArtistNames));
+            console.log('song name: ' + songs[i].name);
+            console.log('preview song: ' + songs[i].preview_url);
+            console.log('album: ' + songs[i].album.name);
+            console.log('*************************');
+        }
+    });
+
+}
+var getMyTweets = function() {
+    var client = new Twitter(keys.twitterKeys);
+
+    var params = {screen_name: 'TylerHunter06'};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            // console.log(tweets);
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log('');
+                console.log(tweets[i].text);
+            }
+        }
+    });
+}
+
+
+var getMeMovie = function(movieName) {
+    if (movieName === undefined) {
+        movieName = "Mr Nobody";
     }
 
-    var tracks = data.tracks.items; //Array of tracks --each track is an object
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=40e9cece";
 
-    for (var i = 0; i < tracks.length; i++ ) {
-        console.log(tracks[i].preview_url);
+    request(urlHit, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var jsonData = JSON.parse(body);
+
+            console.log("Title: " + jsonData.Title);
+            console.log("Year: " + jsonData.Year);
+            console.log("Rated: " + jsonData.Rated);
+            console.log("IMDB Rating: " + jsonData.imdbRating);
+            console.log("Country: " + jsonData.Country);
+            console.log("Language: " + jsonData.Language);
+            console.log("Plot: " + jsonData.Plot);
+            console.log("Actors: " + jsonData.Actors);
+            console.log("Rotton Tomatoes URL: " + jsonData.tomatoURL);
+        }
+    });
+};
+
+var doWhatItSays = function() {
+fs.readFile('random.txt', 'utf8', function (err, data) {
+
+    if (err) throw err;
+
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
     }
-});
+    else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+    }
+    });
+};
 
+var pick = function (caseData, functionData) {
+    switch(caseData) {
+        case 'my-tweets' :
+            getMyTweets();
+            break;
+        case 'spotify-this-song':
+            getMeSpotify(functionData);
+            break;
+        case "movie-this":
+            getMeMovie(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+        console.log('LIRI does not know that');
+    }
+
+}
+
+var runThis = function (argOne, argTwo) {
+    pick(argOne, argTwo);
+
+};
+
+runThis(process.argv[2], process.argv[3]);
